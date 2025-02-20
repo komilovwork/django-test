ebd7f2ea82472e2820c323f12ccece5355d61807
komilovwork <tiskoerors@gmail.com>
2025-02-20 15:47:47 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
 ]
